a5909dcd9a7d692d63ea2b6f8184f28b81425546
YerkoPalma <yerko.palma@usach.cl>
2017-10-20 21:45:57 -0300

    delete some more files

--- views/main.js
+++ views/main.js
@@ -1,6 +1,5 @@
 var html = require('bel')
 var css = require('sheetify')
-const navbar = require('../components/navbar')
 const cover = require('../components/cover')
 
 var container = css`
@@ -14,7 +13,15 @@ function mainView (state) {
   <body class="helvetica w-100 pa2 bg-black min-vh-100">
     <div class="pa3 bg-white w-100 h-100 overflow-y-scroll ${container}">
       <div class="mv4 mv5-lcf w-100">
-        ${navbar(state)}
+        <nav class="db absolute top-0 left-0 dt-l w-100 border-box pa3 ph5-l navbar">
+          <a class="dn db-l db-m db-ns dtc-l v-mid mid-gray link dim w-100 w-25-l tc tl-l mb2 mb0-l" href="#" title="Home">
+            <img src="https://s.gravatar.com/avatar/d809533c5ce964b709e2b38e6573973b?s=240" class="dib w2 h2 br-100" alt="Yerko Palma">
+          </a>
+          <div class="db dtc-l v-mid w-100 w-75-l tc tr-l">
+            <a href="/projects" class="link dim pointer dark-gray tracked-mega fw6 f6 f5-l dib mr3 mr4-l" title="Proyectos">Proyectos</a>
+            <a class="link dim dark-gray tracked-mega fw6 f6 f5-l dib" href="/blog" title="Blog">Blog</a>
+          </div>
+        </nav>
         <div class="mw5 mw7-ns center ph5-ns mh5">
           ${cover(state)}
           <h1 class="near-black lh-copy tc mb0">${state.username}</h1>
