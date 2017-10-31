b6dabe86c3bd1d39a424f7ed6a3e7a495a7b066a
Yerko Palma <yerko.palma@usach.cl>
2017-10-31 17:24:30 +0000

    improve blog index rendering

--- views/posts/index.js
+++ views/posts/index.js
@@ -2,26 +2,37 @@ var md = require('../../lib/md')
 var html = require('choo/html')
 const { container } = require('../../lib/styles')
 
+var posts = [
+  {
+    title: 'Post title 1',
+    link: '/blog/post1',
+    description: `Lorem ipsum dolor sit amen Lorem ipsum dolor sit amen Lorem ipsum dolor sit 
+                  amen Lorem ipsum dolor sit amen Lorem ipsum dolor sit amen ...`
+  },
+  {
+    title: 'Post title 2',
+    link: '/blog/post2',
+    description: `Lorem ipsum dolor sit amen Lorem ipsum dolor sit amen Lorem ipsum dolor sit 
+                  amen Lorem ipsum dolor sit amen Lorem ipsum dolor sit amen ...`
+  }
+]
 var dom = md`
 
-## [Post title 1](/post1)
-
-Lorem ipsum dolor sit amen Lorem ipsum dolor sit amen Lorem ipsum dolor sit 
-amen Lorem ipsum dolor sit amen Lorem ipsum dolor sit amen ...
-
----
-
-## [Post title 2](/post2)
-
-Lorem ipsum dolor sit amen Lorem ipsum dolor sit amen Lorem ipsum dolor sit 
-amen Lorem ipsum dolor sit amen Lorem ipsum dolor sit amen ...
-
+${posts.reduce((_, post) => {
+  return _ + `
+  ## [${post.title}](${post.link})
+  
+  ${post.description}
+  
+  ---
+`
+}, '')}
 `
 dom.classList.add('pa4', 'ph5-l', 'georgia', 'mw9-l', 'center')
 
 function blogView (params, store) {
   return html`<body class="helvetica black w-100 pa2 bg-black min-vh-100">
-    <div class="pa7-l pt6-l pv5 bg-white w-100 h-100 overflow-y-scroll ${container}">
+    <div class="pa7-l pv6-l pv5 bg-white w-100 h-100 overflow-y-scroll ${container}">
       ${dom}
     </div>
   </body>`
